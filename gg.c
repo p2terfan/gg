b97e51e8a908440bffe936d10f62efeccad9258a
p2terfan <p2terfan@gamil.com>
2011-06-16 06:12:00 -0700

    input n and pring 7 * n

--- gg.c
+++ gg.c
@@ -5,9 +5,10 @@ int print_7_n(int n)
 	printf("7 * %d = %d\n",n,7 * n) ;
 }
 int main() {
-	int i ;
-	for(i=1;i<10;i++)
-		print_7_n(i);
+	int i,n ;
+	scanf("%d",&n);
+	//for(i=1;i<10;i++)
+		print_7_n(n);
 	return 0;
 }
 
