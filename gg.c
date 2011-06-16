022dd2799e0b3fdcac8cdfbbff585b38553b7b74
p2terfan <p2terfan@gamil.com>
2011-06-16 05:51:12 -0700

    append from 7 * 2 to 7 * 9

--- gg.c
+++ gg.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main() {
 	printf("7*1 = 7\n");
+	printf("7*2 = 14\n");
+	printf("7*3 = 21\n");
+	printf("7*4 = 28\n");
+	printf("7*5 = 35\n");
+	printf("7*6 = 42\n");
+	printf("7*7 = 49\n");
+	printf("7*8 = 56\n");
+	printf("7*9 = 63\n");
 	return 0;
 }
 
