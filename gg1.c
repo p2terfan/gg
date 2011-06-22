df3437ac194cd04d0db26e5330b91c7883eb84d0
p2terfan <p2terfan@gmail.com>
2011-06-22 00:30:08 -0700

    print from n*1 to n*9

---
+++ gg1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n ;
+	scanf("%d",&n);
+	printf("%d*1=%d\n",n,n*1);
+	printf("%d*2=%d\n",n,n*2);
+	printf("%d*3=%d\n",n,n*3);
+	printf("%d*4=%d\n",n,n*4);
+	printf("%d*5=%d\n",n,n*5);
+	printf("%d*6=%d\n",n,n*6);
+	printf("%d*7=%d\n",n,n*7);
+	printf("%d*8=%d\n",n,n*8);
+	printf("%d*9=%d\n",n,n*9);
+	return 0 ;
+
+}
+
